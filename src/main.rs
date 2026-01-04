93eb15baf6dbe5286678d55124d886c57804ac26
Cymorn <amujosimon01@gmail.com>
2026-01-04 19:28:46 +0100

    i am having problem with the main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,28 @@
-use axum::{routing::get, Router};
+use axum::{
+    routing::get,
+    extract::Path,
+    Router,
+};
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new() //ceates a new empty router
-        .route("/health", get(health_check)); //adds a route
-    let listener =
-    tokio::net::TcpListener::bind("127.0.0.1:3000")
+    let app = Router::new()
+        .route("/health", get(health_check))
+        .route("/wallet/{address}", get(wallet_handler));
+
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
         .await
         .unwrap();
+
     println!("Server running on http://127.0.0.1:3000");
 
-    axum::serve(listener, app).await.unwrap(); //starts the HTTP server
+    axum::serve(listener, app).await.unwrap();
+}
 
-    async fn health_check() -> &'static str {"Ok"}
+async fn health_check() -> &'static str {
+    "OK"
+}
+
+async fn wallet_handler(Path(address): Path<String>) -> String {
+    format!("Wallet address received: {}", address)
 }
